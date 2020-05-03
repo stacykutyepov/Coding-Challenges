f9b1ebe69adefdbc7d90428cf249079f9600e434
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2020-05-03 11:49:49 -0400

    Reorganize

--- JS-Basics/LearnJS.js
+++ JS-Basics/LearnJS.js
@@ -409,38 +409,38 @@ for (var i = 0; i < bills.length; i++) {
 console.log(sumTips / bills.length);
 */
 
-// Differen solution*************
-var john = {
-  fullName: 'John Smith',
-  bills: [124, 48, 268, 180, 42],
-  calcTips: function () {
-    this.tips = [];
-    this.finalValues = [];
-    for (var i = 0; i < this.bills.length; i++) {
-      //determine percentage based on tipping rules
-      var percentage;
-      var bill = this.bills[i];
-      if (bill < 50) {
-        percentage = .2;
-      } else if (bill >= 50 && bill < 200) {
-        percentage = .15;
-      } else {
-        percentage = 0.1
-      }
-      // add results to the corresponding array
-      this.tips[i] = bill * percentage;
-      this.finalValues[i] = bill + bill * percentage;
-    }
-  }
-}
-function caclAverage(tips){
-  var sum = 0;
-  for (var i = 0; i < tips.length; i++){
-    sum = sum + tips[i];
-  }
-  return sum / tips.length;
-}
+// // Differen solution*************
+// var john = {
+//   fullName: 'John Smith',
+//   bills: [124, 48, 268, 180, 42],
+//   calcTips: function () {
+//     this.tips = [];
+//     this.finalValues = [];
+//     for (var i = 0; i < this.bills.length; i++) {
+//       //determine percentage based on tipping rules
+//       var percentage;
+//       var bill = this.bills[i];
+//       if (bill < 50) {
+//         percentage = .2;
+//       } else if (bill >= 50 && bill < 200) {
+//         percentage = .15;
+//       } else {
+//         percentage = 0.1
+//       }
+//       // add results to the corresponding array
+//       this.tips[i] = bill * percentage;
+//       this.finalValues[i] = bill + bill * percentage;
+//     }
+//   }
+// }
+// function caclAverage(tips){
+//   var sum = 0;
+//   for (var i = 0; i < tips.length; i++){
+//     sum = sum + tips[i];
+//   }
+//   return sum / tips.length;
+// }
 
-john.calcTips();
-console.log(john);
-john.average = caclAverage(john.tips);
+// john.calcTips();
+// console.log(john);
+// john.average = caclAverage(john.tips);
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Coding-Challange
+
 Learning JS &amp; Coding-Challenges
- Here I stor my coding challanges and a process
+Here I store my coding challanges and a process
+
+JS-Basics & JS-Basics 02 folders contain Basic Topics of JavaScript;
+
+JSalgorithmsSolved are challenges from FreeCodeCamp Javascript Algorithms and Data Structures Course;
