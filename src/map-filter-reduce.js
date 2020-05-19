11fc34030f96ae5fc0499a68662b17b2cac243c8
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2020-05-19 15:39:53 -0400

    map filter reduce

--- src/map-filter-reduce.js
+++ src/map-filter-reduce.js
@@ -5,18 +5,14 @@
 // passed to it. An example of an array containing only real numbers is [-3, 4.8, 5, 3, -3.2].
 
 const squareList = (arr) => {
-    // Only change code below this line
-    // if (num > 0 && Number.isInteger(num)){
-    // }
+
     const newNum = arr.filter(num => num > 0)
         .filter(num => Number.isInteger(num))
         .reduce((memo, value) => {
             memo.push(value * value)
             return memo
-            //  return (memo + value) * value;
         }, [])
     return newNum;
-    // Only change code above this line
 };
 
 const squaredIntegers = squareList([-3, 4.8, 5, 3, 7, -3.2]);
