31f306bbab6669362a94764849b2a60be4e71bb3
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2020-05-30 14:34:05 -0400

    Return a sentence of the passed binary string.

---
+++ src/freeCodeCamp-Challenges/Binary-Agents.js
@@ -0,0 +1,16 @@
+/*Intermediate Algorithm Scripting: Binary Agents
+
+Return an English translated sentence of the
+ passed binary string.
+
+The binary string will be space separated. */
+
+function binaryAgent(str) {
+    let decimalArr = [];
+    for (let i of str.split(" ")) {
+        decimalArr.push(String.fromCharCode(parseInt(i, 2)))
+    }
+    return decimalArr.join("");
+}
+
+binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111"); //should return "Aren't bonfires fun!?"
